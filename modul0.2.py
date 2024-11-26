39ff8518fce21243b192bc6771cc5f1387f4f28f
VladyTol1995 <tolstosheevvladimir4@gmail.com>
2024-11-26 12:39:21 +0300

    Add files via upload

---
+++ modul0.2.py
@@ -0,0 +1,13 @@
+print(12 + 89 - 1)
+# 1st program
+print(9 ** 0.5 * 5)
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print( 2 * 2 + 2)
+print (2 * (2 +2))
+print ((2*2+2) == (2* (2+2)))
+# 4th program
+print ('123.456')
+print(type ('123.456'))
+print((int((float ('123.456')) * 10)) % 10)
